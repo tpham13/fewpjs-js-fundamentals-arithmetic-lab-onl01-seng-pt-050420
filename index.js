e1b89b1379471e8c8e5410bec958f1dc63339720
tpham13 <tpham0123@gmail.com>
2020-12-03 14:35:51 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+const num1 = 10
+const num2 = 6
+
+const multiply = (num1 * num2) + 2 
+
+const random = Math.random()
+
+const mod = 16 % 6
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
